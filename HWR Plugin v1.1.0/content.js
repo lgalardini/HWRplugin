f3e562de2862a30120655ec6e68dfbd54503f19b
lgalardini <luca.galaghera@gmail.com>
2016-12-17 19:50:24 +0100

    Added current version
    Version v1.1.0 Chorme source code.
    For Firefox it is necessary just edit the manifest.json file.

---
+++ HWR Plugin v1.1.0/content.js
@@ -0,0 +1,43 @@
+﻿/*
+ * HWReady.it Community Extension
+ * Coded by: Luca Galardini
+ * www.facebook.com/GalardiniLuca
+ *
+ *
+ * Amazon Reflink Core by Amazing Affiliate Link
+ * Copyright (c) 2012-2014 Vincent Perta
+ * http://www.linkedin.com/pub/vincent-perta/23/720/4b0
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * http://www.gnu.org/licenses/gpl-3.0.txt 
+ */
+
+//FEATURE DISABLED IN THIS PLUGIN VERSION
+// Find the search textbox that appears on most amazon pages
+/*
+var textbox = document.getElementById('twotabsearchtextbox');
+var searchboxInst = document.getElementById('ig-header-search-box-input');
+var searchboxGear = document.getElementById('js_topSeachInput');
+
+
+
+// If the search textbox is found
+if (textbox) {
+    // Set focus to it, allowing the user to immediately begin typing their search criteria.
+    textbox.focus();
+}
+else if (searchboxInst) {
+    searchboxInst.focus();
+}
+else if(searchboxGear) {
+    searchboxGear.focus();
+}*/
